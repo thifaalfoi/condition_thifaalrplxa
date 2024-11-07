8e6c4d203543a1dc75aa5f95871da6fae522f832
thifaalfoi <family42h@gmail.com>
2024-11-07 13:34:55 +0700

    Add files via upload

---
+++ condition_thifaal_rplxa/script.js
@@ -0,0 +1,13 @@
+document.getElementById("orderButton").onclick = fuction () {
+    let foodPrice = parseInt(document.getElementById ("food").value);
+    let drinkPrice = parseInt(document.getElementById ("drink").value);
+
+    let foodPrice = foodPrice + drinkPrice;
+
+    let discount = 0;
+
+    if (totalPrice > 100) {
+        alert(('ok'))
+    }
+
+}
